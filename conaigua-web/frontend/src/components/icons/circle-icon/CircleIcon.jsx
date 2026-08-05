61be749f7b348874da210090363bac378d4b12aa
CHRISTOPHER CHAPARRO CASTILLO <127803678+Chris6264@users.noreply.github.com>
2026-08-05 06:45:36 -0700

    Delete conaigua-web directory

--- conaigua-web/frontend/src/components/icons/circle-icon/CircleIcon.jsx
+++
@@ -1,9 +0,0 @@
-import "./CircleIcon.css";
-
-export function CircleIcon ({ icon: Icon, variant, className, size}) {
-  return (
-    <div className={`circle-icon circle-icon--${variant} ${className}`}>
-      <Icon size={size} />
-    </div>
-  );
-};
